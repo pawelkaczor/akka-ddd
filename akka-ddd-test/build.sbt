a16f18f885e6f9071c45a55e1de83aa08de92dad
pawelkaczor <newion@o2.pl>
2015-01-14 14:10:25 +0100

    support command generators

---
+++ akka-ddd-test/build.sbt
@@ -0,0 +1,1 @@
+scalacOptions ++= Seq("-language:implicitConversions")
--- akka-ddd-test/src/main/scala/pl/newicom/dddd/test/support/GivenWhenThenTestFixture.scala
+++ akka-ddd-test/src/main/scala/pl/newicom/dddd/test/support/GivenWhenThenTestFixture.scala
@@ -3,6 +3,8 @@ package pl.newicom.dddd.test.support
 import akka.actor.Status.Failure
 import akka.actor.{ActorRef, ActorSystem}
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalacheck.Gen
+import pl.newicom.dddd.aggregate.Command
 import pl.newicom.dddd.delivery.protocol.Acknowledged
 
 import scala.concurrent.duration._
@@ -12,23 +14,32 @@ abstract class GivenWhenThenTestFixture(_system: ActorSystem) extends TestKit(_s
 
   def officeUnderTest: ActorRef
 
+  case class WhenCommand[C <: Command](actual: C, params: Seq[Any] = Seq.empty)
+
+  implicit def toWhenCommand[C <: Command](cGen: Gen[C]): WhenCommand[C] = toWhenCommand(cGen.sample.get)
+  implicit def toWhenCommand[C <: Command](c: C): WhenCommand[C] = WhenCommand(c)
+  implicit def toWhenCommandGen[C <: Command](cGen: Gen[(C, Any)]): WhenCommand[C] = {
+    val (c, param1) = cGen.sample.get
+    WhenCommand(c, List(param1))
+  }
+
   case class Given(givenFun: () => Unit, givenCompleted: () => Boolean = () => true) {
 
-    def whenCommand(command: AnyRef) = when(() => {
-      officeUnderTest ! command
+    def whenCommand[C <: Command](command: WhenCommand[C]): When[C] = when(command, () => {
+      officeUnderTest ! command.actual
     })
 
-    def when(whenFun: () => Unit) = {
+    private def when[C <: Command](command: WhenCommand[C], whenFun: () => Unit): When[C] = {
       givenFun()
       if (givenCompleted()) {
-        When(whenFun)
+        When(command, whenFun)
       } else {
         throw new RuntimeException("Given failed")
       }
     }
   }
 
-  def givenCommand(command: Any) = givenCommands(List(command) :_*)
+  def givenCommand(command: Any): Given = givenCommands(List(command) :_*)
 
   def givenCommands(commands: Any*) = {
     def commandsAcknowledged: Boolean = {
@@ -42,11 +53,22 @@ abstract class GivenWhenThenTestFixture(_system: ActorSystem) extends TestKit(_s
     )
   }
 
-  case class When(whenFun: () => Unit) {
+  case class When[C <: Command](command: WhenCommand[C], whenFun: () => Unit) {
+
+    def expectEvent[E](f: (C) => E)(implicit t: ClassTag[E]): Unit = {
+      expectEvent(f(command.actual))
+    }
+
+    def expectEvent[E](f: (C, Any) => E)(implicit t: ClassTag[E]): Unit = {
+      expectEvent(f(command.actual, command.params(0)))
+    }
 
     def expectEvent[E](e: E)(implicit t: ClassTag[E]): Unit =
       expectEventMatching[E](
-        matcher = { case actual if actual == e => e},
+        matcher = {
+          case actual
+            if actual == e => e
+        },
         hint = e.toString
       )
 
@@ -55,6 +77,10 @@ abstract class GivenWhenThenTestFixture(_system: ActorSystem) extends TestKit(_s
       expectMsgPF[Boolean](3 seconds, hint = s"Failure caused by ${t.runtimeClass.getName} with message $message") {
         case actual @ Failure(ex) if ex.getClass == t.runtimeClass && (message == null || message == ex.getMessage) => true
       }
+    }
+
+    def expectEventMatching2[E](f: (C) => PartialFunction[Any, E], hint: String = "")(implicit t: ClassTag[E]): E = {
+      expectEventMatching(f(command.actual))
     }
 
     def expectEventMatching[E](matcher: PartialFunction[Any, E], hint: String = "")(implicit t: ClassTag[E]): E = {
@@ -66,7 +92,5 @@ abstract class GivenWhenThenTestFixture(_system: ActorSystem) extends TestKit(_s
 
   }
 
-  def when(whenFun: => Unit) = When(() => whenFun)
-
-  def whenCommand(command: AnyRef) = Given(() => ()).whenCommand(command)
+  def whenCommand[C <: Command](c: WhenCommand[C]) = Given(() => ()).whenCommand(c)
 }
