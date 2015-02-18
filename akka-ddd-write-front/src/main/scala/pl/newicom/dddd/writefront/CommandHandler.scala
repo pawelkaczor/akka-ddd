3fe58d884b1a57bbe48f46be43916c832828e3f5
pawelkaczor <newion@o2.pl>
2015-02-18 17:24:47 +0100

    fix http response marshaling (CommandHandler (write-front))

--- akka-ddd-write-front/src/main/scala/pl/newicom/dddd/writefront/CommandHandler.scala
+++ akka-ddd-write-front/src/main/scala/pl/newicom/dddd/writefront/CommandHandler.scala
@@ -1,22 +1,21 @@
 package pl.newicom.dddd.writefront
 
 import akka.actor.Actor
-import akka.http.model.{StatusCode, StatusCodes}
 import akka.pattern.ask
 import akka.util.Timeout
 import pl.newicom.dddd.aggregate.Command
 import pl.newicom.dddd.delivery.protocol.Processed
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 trait CommandHandler extends GlobalOfficeClientSupport {
   this: Actor =>
 
-  def handle(officeName: String, command: Command)(implicit timeout: Timeout, ec: ExecutionContext): Future[StatusCode] = {
+  def handle(officeName: String, command: Command)(implicit timeout: Timeout, ec: ExecutionContext): Future[Try[String]] = {
     office(officeName).ask(command).flatMap {
-      case Processed(Success(_)) => Future(StatusCodes.OK)
-      case Processed(Failure(_)) => Future(StatusCodes.InternalServerError)
+      case Processed(Success(_)) => Future(Success("Command processed. Thank you!"))
+      case Processed(Failure(ex)) => Future(Failure(ex))
     }
   }
 }
