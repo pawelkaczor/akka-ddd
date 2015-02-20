994eb278eabefbe9099c4c0ed5ba79784680266c
pawelkaczor <newion@o2.pl>
2015-02-20 15:10:45 +0100

    minor

--- akka-ddd-write-front/src/main/scala/pl/newicom/dddd/writefront/CommandHandler.scala
+++ akka-ddd-write-front/src/main/scala/pl/newicom/dddd/writefront/CommandHandler.scala
@@ -3,8 +3,8 @@ package pl.newicom.dddd.writefront
 import akka.actor.Actor
 import akka.pattern.ask
 import akka.util.Timeout
-import pl.newicom.dddd.aggregate.Command
 import pl.newicom.dddd.delivery.protocol.Processed
+import pl.newicom.dddd.messaging.command.CommandMessage
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
@@ -12,7 +12,7 @@ import scala.util.{Failure, Success, Try}
 trait CommandHandler extends GlobalOfficeClientSupport {
   this: Actor =>
 
-  def handle(officeName: String, command: Command)(implicit timeout: Timeout, ec: ExecutionContext): Future[Try[String]] = {
+  def handle(officeName: String, command: CommandMessage)(implicit t: Timeout, ec: ExecutionContext): Future[Try[String]] = {
     office(officeName).ask(command).flatMap {
       case Processed(Success(_)) => Future(Success("Command processed. Thank you!"))
       case Processed(Failure(ex)) => Future(Failure(ex))
