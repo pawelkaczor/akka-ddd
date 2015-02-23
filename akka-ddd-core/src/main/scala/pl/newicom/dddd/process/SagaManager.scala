0fe05bc10376a645038ff6aa53906927fe29a9d3
pawelkaczor <newion@o2.pl>
2015-02-23 12:12:23 +0100

    fix ClassCastException

--- akka-ddd-core/src/main/scala/pl/newicom/dddd/process/SagaManager.scala
+++ akka-ddd-core/src/main/scala/pl/newicom/dddd/process/SagaManager.scala
@@ -74,7 +74,10 @@ class SagaManager(sagaConfig: SagaConfig[_], sagaOffice: ActorPath) extends Pers
         log.warning(s"No correlationId. Skipping $em")
       } else {
         deliver(sagaOffice, internalDeliveryId => {
-          val deliveryId = em.getMetaAttribute[Long](DeliveryId)
+          val deliveryId = em.getMetaAttribute[Any](DeliveryId) match {
+            case bigInt: BigInt => bigInt.toLong
+            case l => l.asInstanceOf[Long]
+          }
           log.debug(s"[DELIVERY-ID: $deliveryId] Delivering: $em")
           alodState = alodState.withSent(internalDeliveryId, deliveryId)
           em
