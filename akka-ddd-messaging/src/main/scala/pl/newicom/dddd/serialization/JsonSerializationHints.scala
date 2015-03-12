a59f9aca49d9164a38726a55434d49aed9a7e4ba
pawelkaczor <newion@o2.pl>
2015-03-12 16:01:37 +0100

    allow concatenation of JsonSerializationHints

--- akka-ddd-messaging/src/main/scala/pl/newicom/dddd/serialization/JsonSerializationHints.scala
+++ akka-ddd-messaging/src/main/scala/pl/newicom/dddd/serialization/JsonSerializationHints.scala
@@ -8,4 +8,9 @@ trait JsonSerializationHints[A] {
   def serializers: List[Serializer[_]]
 
   def ++ (formats: Formats): Formats = formats ++ serializers + typeHints
+
+  def ++ (other: JsonSerializationHints[_]): JsonSerializationHints[_] = new Tuple2(typeHints, serializers) with JsonSerializationHints[AnyRef] {
+    override def typeHints: TypeHints = _1 + other.typeHints
+    override def serializers: List[Serializer[_]] = _2 ++ other.serializers
+  }
 }
