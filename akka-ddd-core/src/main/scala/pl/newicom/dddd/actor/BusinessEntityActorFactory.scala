c859b272164931de32f0f305d7f47edb8687690e
pawelkaczor <newion@o2.pl>
2015-06-28 13:10:31 +0200

    [minor] improve "implicit not found" compilation error message

--- akka-ddd-core/src/main/scala/pl/newicom/dddd/actor/BusinessEntityActorFactory.scala
+++ akka-ddd-core/src/main/scala/pl/newicom/dddd/actor/BusinessEntityActorFactory.scala
@@ -3,8 +3,10 @@ package pl.newicom.dddd.actor
 import akka.actor.Props
 import pl.newicom.dddd.aggregate.BusinessEntity
 
+import scala.annotation.implicitNotFound
 import scala.concurrent.duration.Duration
 
+@implicitNotFound("could not find factory for ${A} \nsee: BusinessEntityActorFactory")
 abstract class BusinessEntityActorFactory[A <: BusinessEntity] {
   def props(pc: PassivationConfig): Props
   def inactivityTimeout: Duration
