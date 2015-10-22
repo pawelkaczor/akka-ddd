e6255664b6b156f4f87453ec7f1cd4759ea14eee
pawelkaczor <newion@o2.pl>
2015-10-22 11:02:04 +0200

    [minor]

--- view-update-sql/src/main/scala/pl/newicom/dddd/view/sql/SqlViewHandler.scala
+++ view-update-sql/src/main/scala/pl/newicom/dddd/view/sql/SqlViewHandler.scala
@@ -4,7 +4,6 @@ import com.typesafe.config.Config
 import pl.newicom.dddd.messaging.event.DomainEventMessage
 import pl.newicom.dddd.view.ViewHandler
 import slick.dbio.DBIOAction.sequence
-import slick.dbio.{DBIOAction, NoStream}
 import slick.driver.JdbcProfile
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -15,15 +14,15 @@ class SqlViewHandler(override val config: Config, override val vuConfig: SqlView
 
   private lazy val viewMetadataDao = new ViewMetadataDao
 
-  def handle(eventMessage: DomainEventMessage, eventNumber: Long): Future[Unit] = run {
-    sequence(vuConfig.projections.map(_.consume(eventMessage))) >>
-    viewMetadataDao.insertOrUpdate(viewName, eventNumber)
-  }.mapToUnit
+  def handle(eventMessage: DomainEventMessage, eventNumber: Long): Future[Unit] =
+    viewStore.run {
+      sequence(vuConfig.projections.map(_.consume(eventMessage))) >>
+      viewMetadataDao.insertOrUpdate(viewName, eventNumber)
+    }.mapToUnit
 
-  def lastEventNumber: Future[Option[Long]] = run {
-    viewMetadataDao.lastEventNr(viewName)
-  }
-
-  private def run[R](a: DBIOAction[R, NoStream, Nothing]): Future[R] = viewStore.run(a)
+  def lastEventNumber: Future[Option[Long]] =
+    viewStore.run {
+      viewMetadataDao.lastEventNr(viewName)
+    }
 
 }
