9317a03ed89706806db96dce7945881b423e78b4
pawelkaczor <newion@o2.pl>
2015-12-08 13:10:59 +0100

    [refactor, minor]

--- akka-ddd-core/src/main/scala/pl/newicom/dddd/persistence/ForgettingParticularEvents.scala
+++
@@ -1,9 +0,0 @@
-package pl.newicom.dddd.persistence
-
-import akka.persistence.PersistentActor
-
-trait ForgettingParticularEvents {
-  this: PersistentActor =>
-
-  override def journalPluginId = "akka.persistence.journal.inmem"
-}
--- akka-ddd-core/src/main/scala/pl/newicom/dddd/process/Receptor.scala
+++ akka-ddd-core/src/main/scala/pl/newicom/dddd/process/Receptor.scala
@@ -10,7 +10,7 @@ import pl.newicom.dddd.messaging.event._
 import pl.newicom.dddd.messaging.{Message, MetaData}
 import pl.newicom.dddd.office.LocalOfficeId
 import pl.newicom.dddd.process.ReceptorConfig.{ReceiverResolver, StimuliSource, Transduction}
-import pl.newicom.dddd.persistence.{RegularSnapshottingConfig, RegularSnapshotting, ForgettingParticularEvents}
+import pl.newicom.dddd.persistence.{RegularSnapshottingConfig, RegularSnapshotting}
 
 object ReceptorConfig {
   type Transduction = PartialFunction[EventMessage, Message]
@@ -58,11 +58,14 @@ case class ReceptorBuilder(
   def propagateTo(_receiver: ActorPath) = route({case _ => _receiver})
 }
 
-trait ReceptorPersistencePolicy extends ReceivePipeline with ForgettingParticularEvents with RegularSnapshotting {
+trait ReceptorPersistence extends ReceivePipeline with RegularSnapshotting {
   this: PersistentActor =>
+
+  override def journalPluginId = "akka.persistence.journal.inmem"
+
 }
 
-abstract class Receptor extends AtLeastOnceDeliverySupport with ReceptorPersistencePolicy {
+abstract class Receptor extends AtLeastOnceDeliverySupport with ReceptorPersistence {
   this: EventStreamSubscriber =>
 
   def config: ReceptorConfig
