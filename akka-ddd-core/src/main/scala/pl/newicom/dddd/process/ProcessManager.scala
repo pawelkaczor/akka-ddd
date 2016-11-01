b2dc5a5d362a4a1d34bfddb0391146ef8d8ad032
pawelkaczor <newion@o2.pl>
2016-11-01 09:23:10 +0100

    add processManagerId alias method

--- akka-ddd-core/src/main/scala/pl/newicom/dddd/process/ProcessManager.scala
+++ akka-ddd-core/src/main/scala/pl/newicom/dddd/process/ProcessManager.scala
@@ -1,3 +1,5 @@
 package pl.newicom.dddd.process
 
-abstract class ProcessManager[S <: SagaState[S]] extends Saga with SagaCollaboration with SagaStateHandling[S]
+abstract class ProcessManager[S <: SagaState[S]] extends Saga with SagaCollaboration with SagaStateHandling[S] {
+  def processManagerId = sagaId
+}
