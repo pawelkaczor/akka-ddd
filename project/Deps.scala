18e9d857dd66304936b97e776dfb81471fa9939d
pawelkaczor <newion@o2.pl>
2017-05-28 12:05:04 +0200

    update slick-pg to 0.15.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -12,7 +12,7 @@ object Deps {
     val json4s                = "3.5.0"
 
     val Slick                 = "3.2.0"
-    val PostgresqlSlickExt    = "0.15.0-RC" // Slick 3.2.0
+    val PostgresqlSlickExt    = "0.15.0" // Slick 3.2.0
     val H2Driver              = "1.4.189"
 
     // monitoring
