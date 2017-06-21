1ab085a927618a8b57922ac8a75661532cfc23d4
pawelkaczor <newion@o2.pl>
2017-06-21 21:58:06 +0200

    #50 - fix SchedulerSpec

--- akka-ddd-scheduling/src/test/scala/pl/newicom/dddd/scheduling/SchedulerSpec.scala
+++ akka-ddd-scheduling/src/test/scala/pl/newicom/dddd/scheduling/SchedulerSpec.scala
@@ -36,7 +36,7 @@ class SchedulerSpec extends OfficeSpec[Scheduler](Some(testSystem)) {
       .expect { c =>
         EventScheduled(
           ScheduledEventMetadata(
-            c.businessUnit,
+            schedulingOfficeID.caseRef(businessUnit).id,
             c.target,
             c.deadline.withSecondOfMinute(0).withMillisOfSecond(0),
             c.deadline.getMillis),
