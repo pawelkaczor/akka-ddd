d23c4b23cc66f29304027d8613d1e6f5b0ae1c9d
AndreyLadniy <andrey.ladniy@gmail.com>
2017-10-13 08:19:55 +0300

    Metadata Tags merge test

--- akka-ddd-protocol/src/test/scala/pl/newicom/dddd/messaging/MetaAttributeSpec.scala
+++ akka-ddd-protocol/src/test/scala/pl/newicom/dddd/messaging/MetaAttributeSpec.scala
@@ -53,5 +53,31 @@ class MetaAttributeSpec extends org.scalatest.FunSuite {
     assert(md != null)
   }
 
+  test("merge meta data empty tags") {
+    val event = MetaData.empty.withAttr(Id, "e-1")
+    val command = MetaData.empty.withAttr(Id, "c-1")
+    val md = MetaDataPropagationPolicy.onCommandSentByPM(event, command)
+    assert(md.tryGet(Tags).exists(_ == Set.empty[String]))
+  }
 
+  test("merge meta data not empty tags on left and empty on right") {
+    val event = MetaData.empty.withAttr(Id, "e-1").withAttr(Tags, Set("t1", "t2"))
+    val command = MetaData.empty.withAttr(Id, "c-1")
+    val md = MetaDataPropagationPolicy.onCommandSentByPM(event, command)
+    assert(md.tryGet(Tags).exists(_ == Set("t1", "t2")))
+  }
+
+  test("merge meta data empty tags on left and non empty on right") {
+    val event = MetaData.empty.withAttr(Id, "e-1")
+    val command = MetaData.empty.withAttr(Id, "c-1").withAttr(Tags, Set("t2", "t3"))
+    val md = MetaDataPropagationPolicy.onCommandSentByPM(event, command)
+    assert(md.tryGet(Tags).exists(_ == Set("t2", "t3")))
+  }
+
+  test("merge meta data non empty tags") {
+    val event = MetaData.empty.withAttr(Id, "e-1").withAttr(Tags, Set("t1", "t2"))
+    val command = MetaData.empty.withAttr(Id, "c-1").withAttr(Tags, Set("t2", "t3"))
+    val md = MetaDataPropagationPolicy.onCommandSentByPM(event, command)
+    assert(md.tryGet(Tags).exists(_ == Set("t1", "t2", "t3")))
+  }
 }
